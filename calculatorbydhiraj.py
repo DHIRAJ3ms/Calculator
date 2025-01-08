5d35d44beadce48a1303b3b09e5e2c8ce102ac47
DHIRAJ3ms <msdhiraj333@gmail.com>
2025-01-08 17:38:20 +0530

    Add files via upload

---
+++ calculatorbydhiraj.py
@@ -0,0 +1,13 @@
+while True:
+    x=int(input("enter the number: "))
+    y=int(input("enter the number: "))
+    z=input("add or multiply or divide or sub: ")
+    if z=='add':
+        print('ans:-',x+y)
+    elif z == 'multiply':
+        print('ans:-',x*y)
+    elif z=='sub':
+        print('ans:-',x-y)
+    elif z=='divide':
+        print('ans:-',x/y)
+    
